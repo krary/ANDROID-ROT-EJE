909e8ff6b1fcbe5f7a0b6ae32399398f0376af12
unknown <claudiogonzales045@gmail.com>
2025-08-03 17:22:04 +0200

    upgrade

---
+++ Kotlin-AndroidStudio-Project/Project Android/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.myapplication
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.media.MediaPlayer
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val botonExit = findViewById<Button>(R.id.b_Message)
+        botonExit.setOnClickListener{
+           // finishAffinity()
+            val mp = MediaPlayer.create(this,R.raw.sonido);
+            mp.start()
+        }
+
+    }
+}
